18a6401c3daf76118159af160bd2f816bc90d366
Roalex86180 <alexreimi@gmail.com>
2025-05-09 15:08:22 -0400

    actualizando version requirements.txt

--- app_consultas.py
+++ app_consultas.py
@@ -5,8 +5,6 @@ import pandas as pd
 import subprocess
 import sys
 
-installed = subprocess.check_output([sys.executable, "-m", "pip", "freeze"])
-print("PAQUETES INSTALADOS EN ENTORNO:", installed.decode())
 
 # --- Función para Conectar a la Base de Datos ---
 # --- Función para Conectar a la Base de Datos (DIRECTO A CLOUD SQL) ---
